7be8c6678a8a42d79888422d9fe7c9d22e97af1c
Abhinay Singh <abhinaysingh0707@gmail.com>
2025-03-04 16:58:17 +0530

    add header

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import Header from '../../components/Header/Header'
 import './Home.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  const [category, setCategory] = useState("ALL");
+
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+    </div>
   )
 }
 
